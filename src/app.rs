c3c75b3bab8eb54f46a87900c3192b33ca45d354
Iaroslav <zanturik@gmail.com>
2024-11-07 12:19:18 -0500

    failed to execute setStartAfter

--- src/app.rs
+++ src/app.rs
@@ -8,7 +8,7 @@ use tokio::time::{self, Duration};
 #[server(GetData, "/api", "GetJson", "getData")]
 async fn load_data(value: i32) -> Result<i32, ServerFnError> {
     dbg!(format!("executing load_data with {} value", &value));
-    // time::sleep(Duration::from_secs(value.try_into().unwrap())).await;
+    time::sleep(Duration::from_secs(value.try_into().unwrap())).await;
     Ok(value + 10)
 }
 
@@ -22,7 +22,7 @@ pub fn App() -> impl IntoView {
         <header>
             <nav>
                 <a href="/">Home</a>
-                <a href="/products/1">Product #1</a>
+                <a href="/products/1/1">Product #1</a>
             </nav>
         </header>
         <div class="container">
@@ -69,7 +69,6 @@ pub fn ShowProduct() -> impl IntoView {
             <Show when=move || product.get().is_some()>
                 <Meta name="description" content=move || product.get().unwrap_or_default().to_string()/>
             </Show>
-
             <Outlet/>
         </Suspense>
     }
@@ -87,7 +86,6 @@ pub fn TestProduct() -> impl IntoView {
             <Show when=move || product.get().is_some()>
                 <Meta name="name" content=move || product.get().unwrap_or_default().to_string()/>
             </Show>
-
             <AnotherTestProduct/>
         </Suspense>
     }
@@ -95,7 +93,7 @@ pub fn TestProduct() -> impl IntoView {
 
 #[component]
 pub fn AnotherTestProduct() -> impl IntoView {
-    let (pr_id, _) = create_signal(55);
+    let (pr_id, _) = create_signal(5);
     let product = create_blocking_resource(pr_id, |id| async move {
         load_data(id).await.unwrap_or_default()
     });
