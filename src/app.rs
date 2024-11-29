54bac2ac96e2c102e0446c3526acc58fd99f7e1f
Iaroslav <zanturik@gmail.com>
2024-11-29 11:55:21 -0500

    Getting context not found error if route is not found

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,9 @@
 use leptos::prelude::*;
-#[cfg(feature = "ssr")]
-use leptos_axum::ResponseOptions;
-use leptos_meta::{provide_meta_context, MetaTags, Stylesheet};
 use leptos_router::{
-    components::{Route, Router, Routes},
-    SsrMode, StaticSegment,
+    components::{FlatRoutes, Route, Router},
+    StaticSegment,
 };
-use reactive_stores::Store;
-use serde::{Deserialize, Serialize};
+
 #[derive(Clone)]
 pub struct FakePool {
     pub id: i32,
@@ -21,8 +17,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <AutoReload options=options.clone() />
-                <HydrationScripts options/>
-                <MetaTags/>
+                <HydrationScripts options islands=true/>
             </head>
             <body>
                 <App/>
@@ -31,100 +26,56 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
     }
 }
 
-#[derive(Debug, Store, Serialize, Deserialize, Default)]
-pub struct StoreData {
-    product: Option<String>,
+#[server(GetData, "/api", "GetJson", "getData")]
+pub async fn get_data() -> Result<Vec<String>, ServerFnError> {
+    let pool = use_context::<FakePool>().ok_or(ServerFnError::new("context not found"))?;
+    Ok(vec![format!("This is the data # {}", pool.id)])
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    // leptos::logging::log!("App loaded...");
-    provide_meta_context();
-
-    let store = Store::new(StoreData::default());
-    provide_context(store);
+    let data = Resource::new_blocking(
+        || (),
+        move |_| async move { get_data().await.unwrap_or_else(|e| vec![e.to_string()]) },
+    );
+    provide_context(data);
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/leptos-resources.css"/>
-        <header>
-            <nav>
-                <a href="/">Home</a>
-                <a href="/products/1/1">Product #1</a>
-            </nav>
-        </header>
-        <div class="container">
         <Router>
-                <Routes fallback=|| "Page not found.".into_view()>
-                    <Route ssr=SsrMode::PartiallyBlocked path=StaticSegment("/") view=ShowProduct/>
-                </Routes>
+            <HeaderComponent/>
+            <main style="background-color: lightblue; padding: 10px">
+            <FlatRoutes fallback=|| "Not found.">
+                <Route path=StaticSegment("") view=HomePage/>
+            </FlatRoutes>
+            </main>
         </Router>
-    </div>
-
     }
 }
 
 #[component]
-pub fn ShowProduct() -> impl IntoView {
-    let store =
-        use_context::<Store<StoreData>>().expect("App is not loaded. Store context not found!");
+fn HeaderComponent() -> impl IntoView {
+    let context_data = use_context::<Resource<Vec<String>>>().expect("Resource context not found!");
 
-    let product = Resource::new_blocking(
-        || (),
-        move |_| async move { String::from("You shell no pass!") },
-    );
+    let data = Suspend::new(async move {
+        let super_string = context_data.await.first().cloned().unwrap_or_default();
 
-    let show_product = Suspend::new(async move {
-        let prod = product.await;
-        store.product().set(Some(prod.clone()));
-        view! { <h1>"Unsere reihe:" { prod }</h1>}
+        view! {
+            <h2>Here we are!!!</h2>
+            <b>{ super_string }</b>
+        }
     });
 
     view! {
-
-        { show_product }
-        <b>Main component</b>
-        <TestProduct/>
-
+        <h1>This is the header</h1>
+        <b>{ data }</b>
+        <h3>Did you see the data?</h3>
     }
 }
 
+/// Renders the home page of your application.
 #[component]
-pub fn TestProduct() -> impl IntoView {
-    let store =
-        use_context::<Store<StoreData>>().expect("App is not loaded. Store context not found!");
-
-    let variant = Resource::new_blocking(
-        move || store.product().get(),
-        |p| async move {
-            match p {
-                Some(s) => get_variants(s).await.unwrap_or_default(),
-                None => String::from("No variant No cry!"),
-            }
-        },
-    );
-
+fn HomePage() -> impl IntoView {
     view! {
-        <Suspense>
-        { variant }
-        <b>Just a test</b>
-         </Suspense>
+        <h4>"Welcome to Leptos!"</h4>
     }
-}
-
-#[server(GetVariants, "/api", "GetJson", "getVariants")]
-pub async fn get_variants(sl: String) -> Result<String, ServerFnError> {
-    let _db_pool = use_context::<FakePool>();
-    let _response = expect_context::<ResponseOptions>();
-    let _db_pool2 = use_context::<FakePool>();
-    let _response3 = expect_context::<ResponseOptions>();
-    let _db_pool4 = use_context::<FakePool>();
-    let _response5 = expect_context::<ResponseOptions>();
-    let _db_pool6 = use_context::<FakePool>();
-    let _response7 = expect_context::<ResponseOptions>();
-    let _db_pool8 = use_context::<FakePool>();
-    let _response9 = expect_context::<ResponseOptions>();
-    let _db_pool10 = use_context::<FakePool>();
-    let _response11 = expect_context::<ResponseOptions>();
-
-    Ok(sl + " bazinga!")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     use axum::Router;
-    use leptos::prelude::*;
+    use leptos::{logging::log, prelude::*};
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use leptos_resources::app::*;
+    use leptos_islands_07_clean::app::*;
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved to deployment
     let conf = get_configuration(None).unwrap();
+    let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr;
+    // Generate the list of routes in your Leptos App
     let routes = generate_route_list(App);
 
-    // build our application with a route
     let app = Router::new()
         .leptos_routes_with_context(
             &leptos_options,
@@ -30,8 +24,10 @@ async fn main() {
         .fallback(leptos_axum::file_and_error_handler(shell))
         .with_state(leptos_options);
 
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    log!("listening on http://{}", &addr);
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    leptos::logging::log!("listening on http://{}", &addr);
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
@@ -40,6 +36,6 @@ async fn main() {
 #[cfg(not(feature = "ssr"))]
 pub fn main() {
     // no client-side main function
-    // unless we want this to work with e.g., Trunk for a purely client-side app
+    // unless we want this to work with e.g., Trunk for pure client-side testing
     // see lib.rs for hydration function instead
 }
